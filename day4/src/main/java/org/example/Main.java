0f080a9d4c9fdcb2e1d3a6e04a4ed7ccfbbc2c01
Uldis1992 <162179052+Uldis1992@users.noreply.github.com>
2024-03-11 20:48:57 +0200

    uzdevums2

--- day4/src/main/java/org/example/Main.java
+++ day4/src/main/java/org/example/Main.java
@@ -1,21 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        task2();
 
-        public static void sample1() {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Sveiki, ievadiet simbolu virkni");
-            String input = sc.nextLine();
-            String result = "Programmēšana";
-            if (input.equals(result)) {
-                System.out.println("Uzvarēji!");
-            } else if (input.length() == result.length()) {
-                System.out.println("Vienāds simbolu skaits");
-            } else{
-                System.out.println("Zaudēji!");
-            }
+
+    public static void sample1() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sveiki, ievadiet simbolu virkni");
+        String input = sc.nextLine();
+        String result = "Programmēšana";
+        if (input.equals(result)) {
+            System.out.println("Uzvarēji!");
+        } else if (input.length() == result.length()) {
+            System.out.println("Vienāds simbolu skaits");
+        } else {
+            System.out.println("Zaudēji!");
+
         }
     }
+}
+
+    private static void task2() {
+        task2();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sveiki, lūdzu, Ievadiet skaitli:");
+
+        int input = sc.nextInt();
+
+        if((input >= 10) || (input <= 5)){
+            System.out.println("$$");
+        }else if((input >= 6) && (input <= 9)) {
+            System.out.println("$");
+
+        }
+    }
+}
